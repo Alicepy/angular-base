0bc92e1f227a073e70f696a9ab2fa64702212b77
chenpeiyu <1499104236@qq.com>
2020-12-17 19:37:09 +0800

    feat cpy 删除测试相关代码

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+    constructor(){}
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        
+    }
+}
