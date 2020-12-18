dd40b2811c1011e05ef16ead5264df81e093d781
chenpeiyu <1499104236@qq.com>
2020-12-18 11:15:24 +0800

    feat： cpy 设置路由&打包路径

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{
-        path:'header',component:HeaderComponent
-    }
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes,{useHash:true})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { importExpr } from "@angular/compiler/src/output/output_ast";
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
